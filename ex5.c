33eaf697838a14cd51e7a840154e1a1767d505d0
Gal Giladi <83518959+SlowlyFire@users.noreply.github.com>
2022-01-06 14:22:38 +0200

    deleted the function comment

--- ex5.c
+++ ex5.c
@@ -14,11 +14,6 @@ typedef struct {
     // int num;
 } pixel_sum;
  
-
-/* Compute min and max of two integers, respectively */
-	//int min(int a, int b) { return (a < b ? a : b); }
-	//int max(int a, int b) { return (a > b ? a : b); }
-
 int calcIndex(int i, int j, int n) {
 	return ((i)*(n)+(j));
 }
